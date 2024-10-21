97b966b5923dea8ee46831a6471b8eadbf9acefc
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-10-21 06:15:21 -0700

    install: add rudimentary "installer"

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,35 @@ mod env_command;
 mod which_command;
 mod yes_command;
 
+/// Binaries that can be installed with --install
+/// Example: ln -sf /full/path/to/rizzybox /usr/local/bin/cat
+const INSTALLABLE_BINS: [&str; 5] = ["cat", "echo", "env", "which", "yes"];
+
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(
+    version,
+    about,
+    long_about = r#"
+
+8888888b.  d8b                            888888b.
+888   Y88b Y8P                            888  "88b
+888    888                                888  .88P
+888   d88P 888 88888888 88888888 888  888 8888888K.   .d88b.  888  888
+8888888P"  888    d88P     d88P  888  888 888  "Y88b d88""88b `Y8bd8P'
+888 T88b   888   d88P     d88P   888  888 888    888 888  888   X88K
+888  T88b  888  d88P     d88P    Y88b 888 888   d88P Y88..88P .d8""8b.
+888   T88b 888 88888888 88888888  "Y88888 8888888P"   "Y88P"  888  888
+                                      888
+                                 Y8b d88P
+                                  "Y88P"
+"#
+)]
 struct Cli {
     #[command(subcommand)]
     command: Option<Commands>,
+
+    #[arg(long)]
+    install: bool,
 }
 
 #[derive(Subcommand)]
@@ -66,11 +90,9 @@ fn main() {
     let binary_name = args.first().map(|s| s.as_str()).unwrap_or_default();
 
     // determine if invoked as subcommand directly: `/bin/echo`
-    let args = if binary_name.ends_with("cat")
-        || binary_name.ends_with("echo")
-        || binary_name.ends_with("env")
-        || binary_name.ends_with("which")
-        || binary_name.ends_with("yes")
+    let args = if INSTALLABLE_BINS
+        .iter()
+        .any(|&bin| binary_name.ends_with(bin))
     {
         // shift binary name to subcommand name
         let subcommand_name = binary_name.split('/').last().unwrap_or(binary_name);
@@ -81,7 +103,21 @@ fn main() {
         args
     };
 
-    let cli = Cli::parse_from(args);
+    let cli = Cli::parse_from(args.clone());
+    // TODO: write actual install logic instead of providing a script
+    if cli.install {
+        println!("# to install rizzybox bins, paste the following in your shell:\n");
+        // `export` works with more shells, like fish
+        println!("export RIZZYBOX_INSTALL_DIR=/usr/local/bin # change this to the desired installation path");
+        for bin in INSTALLABLE_BINS.iter() {
+            println!(
+                "ln -sf {}/{} $RIZZYBOX_INSTALL_DIR/{}",
+                std::env::current_dir().unwrap().display(),
+                args.first().unwrap(),
+                bin,
+            )
+        }
+    }
 
     if let Some(command) = cli.command {
         match command {
