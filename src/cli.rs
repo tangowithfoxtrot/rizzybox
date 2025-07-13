f0d955c912dbfd48a9e8102eeeb33c6a4632d3f0
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2025-07-12 18:18:42 -0700

    style: move cli out of main; better help text

---
+++ src/cli.rs
@@ -0,0 +1,320 @@
+use std::path::PathBuf;
+
+use clap::{ArgAction, Parser, Subcommand};
+use clap_complete::Shell;
+use rizzybox::parse_kv_pair;
+
+use crate::command::uname::IsaFormat;
+
+#[derive(Parser)]
+#[command(
+    version,
+    about = "A multi-call binary containing common coreutils",
+    long_about = r#"
+
+8888888b.  d8b                            888888b.
+888   Y88b Y8P                            888  "88b
+888    888                                888  .88P
+888   d88P 888 88888888 88888888 888  888 8888888K.   .d88b.  888  888
+8888888P"  888    d88P     d88P  888  888 888  "Y88b d88""88b `Y8bd8P'
+888 T88b   888   d88P     d88P   888  888 888    888 888  888   X88K
+888  T88b  888  d88P     d88P    Y88b 888 888   d88P Y88..88P .d8""8b.
+888   T88b 888 88888888 88888888  "Y88888 8888888P"   "Y88P"  888  888
+                                      888
+                                 Y8b d88P
+                                  "Y88P"
+"#
+)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+
+    /// print install script
+    #[arg(long)]
+    pub install: bool,
+
+    /// print install script for sudo
+    #[arg(long)]
+    pub install_with_sudo: bool,
+
+    /// list included binaries
+    #[arg(long)]
+    pub list: bool,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Display machine architecture
+    Arch {},
+
+    /// Print NAME with any leading directory components removed
+    Basename {
+        /// support multiple arguments and treat each as a NAME
+        #[arg(long = "multiple", short = 'a', visible_short_alias = 'm')]
+        multiple: bool,
+
+        /// the NAME of the directory to use
+        name: Vec<String>,
+
+        #[arg(long, short, help = "remove a trailing SUFFIX; implies -a")]
+        suffix: Option<String>,
+
+        /// end each output line with NUL, not newline
+        #[arg(long, short, visible_short_alias = '0')]
+        zero: bool,
+    },
+
+    /// Concatenate and print file contents
+    Cat {
+        /// file to concatenate
+        file: Vec<String>,
+
+        /// language to use for syntax highlighting
+        #[arg(long, short, default_value = "txt")]
+        language: String,
+
+        /// theme to use for colored output
+        #[arg(long, short, default_value = "Dracula")]
+        theme: String,
+
+        /// show non-printable characters
+        #[arg(long, short = 'A', default_value = "false")]
+        show_all: bool,
+
+        /// list available themes
+        #[arg(long)]
+        list_themes: bool,
+
+        /// number all output lines
+        #[arg(long, short)]
+        number_lines: bool,
+    },
+
+    /// Clear the terminal screen
+    Clear {},
+
+    /// Generate completions for your shell
+    Completions {
+        /// the shell to generate completions for
+        shell: Option<Shell>,
+    },
+
+    /// Output each NAME with its last non-slash component and trailing slashes
+    /// removed; if NAME contains no /'s, output '.' (meaning the current directory).
+    #[clap(verbatim_doc_comment)]
+    Dirname {
+        #[arg(required = true)]
+        name: Vec<String>,
+
+        /// end each output line with NUL, not newline
+        #[arg(long, short, visible_short_alias = '0')]
+        zero: bool,
+    },
+
+    /// Write arguments to standard output
+    Echo {
+        /// disable interpretation of backslash escapes
+        #[arg(short = 'E', default_value_t = true)]
+        disable_backslash_escapes: bool,
+
+        /// enable interpretation of backslash escapes
+        #[arg(short = 'e', default_value_t = false)]
+        enable_backslash_escapes: bool,
+
+        /// language to use for syntax highlighting
+        #[arg(long, short, default_value = "txt")]
+        language: String,
+
+        /// do not output a trailing newline
+        #[arg(long, short, default_value_t = false)]
+        nonewline: bool,
+
+        #[arg(default_value = "")]
+        string: Vec<String>,
+
+        /// theme to use for colored output
+        #[arg(long, short, default_value = "Dracula")]
+        theme: String,
+    },
+
+    /// Run a program in a modified environment
+    Env {
+        /// pass ARG as the zeroth argument of COMMAND
+        #[arg(long, short)]
+        argv0: Option<String>,
+
+        /// change working directory to DIR
+        #[arg(long, short)]
+        chdir: Option<String>,
+
+        /// start with an empty environment
+        #[arg(long, short = 'i')]
+        ignore_environment: bool,
+
+        /// end echo output line with NUL, not newline
+        #[arg(long, short = '0', visible_alias = "zero", visible_short_alias = '0')]
+        null: bool,
+
+        /// remove variable from the environment
+        #[arg(action = ArgAction::Append, long, short)]
+        unset: Vec<String>,
+
+        /// KEY=VALUE to set in the environment
+        #[arg(value_parser = parse_kv_pair)]
+        kv_pair: Vec<String>,
+
+        /// command to run in the environment
+        #[arg(last = true)]
+        command: Vec<String>,
+        // FIXME: `last` requires the command to be passed via `--`, which differs from coreutils env
+    },
+
+    ///  Convert tabs in each FILE to spaces, writing to standard output
+    Expand {
+        /// file to concatenate
+        #[arg(default_value = "-")]
+        file: String,
+
+        /// have tabs N characters apart, not 8
+        #[arg(long, short, value_name = "N,LIST", value_delimiter = ',', num_args = 1..)]
+        tabs: Vec<String>,
+    },
+
+    /// Do nothing and exit with a failure status
+    False {},
+
+    /// List information about the FILEs (the current directory by default)
+    Ls {
+        /// do not ignore entries starting with '.'
+        #[arg(long, short)]
+        all: bool,
+
+        /// the PATH to list
+        #[arg(default_value = ".", hide_default_value = true)]
+        path: String,
+    },
+
+    /// Create directories if they do not already exist
+    Mkdir {
+        /// directories to create
+        #[arg(required = true)]
+        dirs: Vec<PathBuf>,
+
+        /// create parent directories as needed
+        #[arg(long, short, env = "RZ_MKDIR_PARENTS")]
+        parents: bool,
+    },
+
+    /// Print the number of cores available to the current process
+    Nproc {
+        /// print the number of cores available to the system
+        #[arg(long, short)]
+        all: bool,
+
+        /// ignore up to N cores
+        #[arg(
+            long,
+            short,
+            value_name = "N",
+            hide_default_value = true,
+            default_value_t = 0
+        )]
+        ignore: usize,
+
+        /// maximum threads to report
+        #[arg(long, env = "OMP_NUM_LIMIT")]
+        omp_num_limit: Option<usize>,
+
+        /// minimum threads to report
+        #[arg(long, env = "OMP_NUM_THREADS")]
+        omp_num_threads: Option<usize>,
+    },
+
+    /// An incomplete shell
+    Sh {},
+
+    /// Pause for NUMBER of seconds
+    Sleep {
+        /// NUMBER of seconds to sleep
+        number: String,
+    },
+
+    /// Reduce word(s) to their stem(s)
+    #[clap(disable_help_subcommand = true)] // someone may pass 'help' as a word
+    Stem {
+        /// do not output a trailing newline
+        #[arg(long, short, default_value_t = false)]
+        nonewline: bool,
+
+        /// words that you would like to stem
+        words: Vec<String>,
+    },
+
+    /// Do nothing and exit with a success status
+    True {},
+
+    /// Print system information
+    Uname {
+        /// print all information
+        #[arg(long, short, default_value_t = false)]
+        all: bool,
+
+        ///print the kernel name
+        #[arg(long, short = 's')]
+        kernel: bool,
+
+        /// print the network node hostname
+        #[arg(long, short = 'n', default_value_t = false)]
+        nodename: bool,
+
+        /// print the kernel release
+        #[arg(long = "kernel-release", short = 'r', default_value_t = false)]
+        kernel_release: bool,
+
+        /// print the kernel version
+        #[arg(long = "kernel-version", short = 'v', default_value_t = false)]
+        kernel_version: bool,
+
+        /// print the machine hardware name
+        #[arg(long, short, default_value_t = false)]
+        machine: bool,
+
+        /// print the operating system
+        #[arg(long, short, default_value_t = false)]
+        operating_system: bool,
+
+        /// the ISA format to use for CPU info
+        #[arg(long, short, default_value_t)]
+        isa_format: IsaFormat,
+    },
+
+    /// Write the full path of COMMAND to standard output
+    /* TODO: GNU which accepts multiple commands */
+    Which {
+        /// print all matching pathnames of each argument
+        #[arg(short, default_value_t = false)]
+        all_occurrences: bool,
+
+        /// command to search for in PATH
+        command: String,
+
+        /// silently return 0 if all of the executables were found or 1 otherwise
+        #[arg(short, default_value_t = false)]
+        silent: bool,
+    },
+
+    /// Repeatedly output lines with TEXT
+    #[clap(disable_help_subcommand = true)] // in case someone passes 'help' as a word
+    Yes {
+        /// AMOUNT of TEXT to output
+        #[arg(long, short, default_value = "0", group = "yes_group")]
+        amount: usize,
+
+        /// output TEXT for a DURATION in seconds
+        #[arg(long, short, group = "yes_group")]
+        duration: Option<f32>, // Optional because ArgGroup requiring one or the other Arg from the group
+
+        #[arg(default_value = "y")]
+        text: String,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,394 +1,36 @@
-use std::{env, path::PathBuf};
-
-use anyhow::{bail, Context, Result};
-use clap::{ArgAction, CommandFactory, Parser, Subcommand};
-use clap_complete::Shell;
-use rizzybox::{consts::INSTALLABLE_BINS, parse_kv_pair};
-
-use crate::command::{
-    basename::basename_command,
-    cat::cat_command,
-    clear::clear_command,
-    dirname::dirname_command,
-    echo::echo_command,
-    env::env_command,
-    expand::expand_command,
-    ls::ls_command,
-    mkdir::mkdir_command,
-    nproc::nproc_command,
-    r#false::false_command,
-    r#true::true_command,
-    sh::sh_command,
-    sleep::sleep_command,
-    stem::stem_command,
-    uname::{arch_command, uname_command, IsaFormat},
-    which::which_command,
-    yes::yes_command,
-};
-
+mod cli;
 mod command;
 
-#[derive(Parser)]
-#[command(
-    version,
-    about = "A multi-call binary containing common coreutils",
-    long_about = r#"
+use std::env;
 
-8888888b.  d8b                            888888b.
-888   Y88b Y8P                            888  "88b
-888    888                                888  .88P
-888   d88P 888 88888888 88888888 888  888 8888888K.   .d88b.  888  888
-8888888P"  888    d88P     d88P  888  888 888  "Y88b d88""88b `Y8bd8P'
-888 T88b   888   d88P     d88P   888  888 888    888 888  888   X88K
-888  T88b  888  d88P     d88P    Y88b 888 888   d88P Y88..88P .d8""8b.
-888   T88b 888 88888888 88888888  "Y88888 8888888P"   "Y88P"  888  888
-                                      888
-                                 Y8b d88P
-                                  "Y88P"
-"#
-)]
-struct Cli {
-    #[command(subcommand)]
-    command: Option<Commands>,
+use anyhow::{bail, Context, Result};
+use clap::{CommandFactory, Parser};
+use clap_complete::Shell;
+use rizzybox::consts::INSTALLABLE_BINS;
 
-    #[arg(long, help = "print install script")]
-    install: bool,
-
-    #[arg(long, help = "print install script for sudo")]
-    install_with_sudo: bool,
-
-    #[arg(long, help = "list included binaries")]
-    list: bool,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    Arch {},
-    Basename {
-        #[arg(
-            long = "multiple",
-            short = 'a',
-            help = "support multiple arguments and treat each as a NAME",
-            visible_short_alias = 'm'
-        )]
-        multiple: bool,
-
-        #[arg(help = "print NAME with any leading directory components removed")]
-        name: Vec<String>,
-
-        #[arg(long, short, help = "remove a trailing SUFFIX; implies -a")]
-        suffix: Option<String>,
-
-        #[arg(
-            long,
-            short,
-            help = "end each output line with NUL, not newline",
-            visible_short_alias = '0'
-        )]
-        zero: bool,
+use {
+    cli::{Cli, Commands},
+    command::{
+        basename::basename_command,
+        cat::cat_command,
+        clear::clear_command,
+        dirname::dirname_command,
+        echo::echo_command,
+        env::env_command,
+        expand::expand_command,
+        ls::ls_command,
+        mkdir::mkdir_command,
+        nproc::nproc_command,
+        r#false::false_command,
+        r#true::true_command,
+        sh::sh_command,
+        sleep::sleep_command,
+        stem::stem_command,
+        uname::{arch_command, uname_command},
+        which::which_command,
+        yes::yes_command,
     },
-    Cat {
-        #[arg(help = "file to concatenate")]
-        file: Vec<String>,
-
-        #[arg(
-            long,
-            short,
-            default_value = "txt",
-            help = "language to use for syntax highlighting"
-        )]
-        language: String,
-
-        #[arg(
-            long,
-            short,
-            default_value = "Dracula",
-            help = "theme to use for colored output"
-        )]
-        theme: String,
-
-        #[arg(
-            long,
-            short = 'A',
-            default_value = "false",
-            help = "show non-printable characters"
-        )]
-        show_all: bool,
-
-        #[arg(long, help = "list available themes")]
-        list_themes: bool,
-
-        #[arg(long, short, help = "number all output lines")]
-        number_lines: bool,
-    },
-    Clear {},
-    Completions {
-        #[arg(help = "the shell to generate completions for")]
-        shell: Option<Shell>,
-    },
-    #[command(
-        about = "Output each NAME with its last non-slash component and trailing slashes
-removed; if NAME contains no /'s, output '.' (meaning the current directory)."
-    )]
-    Dirname {
-        #[arg(help = "strip last component from file name", required = true)]
-        name: Vec<String>,
-
-        #[arg(
-            long,
-            short,
-            help = "end each output line with NUL, not newline",
-            visible_short_alias = '0'
-        )]
-        zero: bool,
-    },
-    Echo {
-        #[arg(
-            short = 'E',
-            default_value_t = true,
-            help = "disable interpretation of backslash escapes"
-        )]
-        disable_backslash_escapes: bool,
-
-        #[arg(
-            short = 'e',
-            default_value_t = false,
-            help = "enable interpretation of backslash escapes"
-        )]
-        enable_backslash_escapes: bool,
-
-        #[arg(
-            long,
-            short,
-            default_value = "txt",
-            help = "language to use for syntax highlighting"
-        )]
-        language: String,
-
-        #[arg(
-            long,
-            short,
-            default_value_t = false,
-            help = "do not output a trailing newline"
-        )]
-        nonewline: bool,
-
-        #[arg(default_value = "")]
-        string: Vec<String>,
-
-        #[arg(
-            long,
-            short,
-            default_value = "Dracula",
-            help = "theme to use for colored output"
-        )]
-        theme: String,
-    },
-    #[command(about = "run a program in a modified environment")]
-    Env {
-        #[arg(long, short, help = "pass ARG as the zeroth argument of COMMAND")]
-        argv0: Option<String>,
-
-        #[arg(long, short, help = "change working directory to DIR")]
-        chdir: Option<String>,
-
-        #[arg(
-            action = clap::ArgAction::SetTrue,
-            long,
-            short = 'i',
-            help = "start with an empty environment",
-        )]
-        ignore_environment: bool,
-
-        #[arg(
-            long,
-            short = '0',
-            help = "end echo output line with NUL, not newline",
-            visible_alias = "zero",
-            visible_short_alias = '0'
-        )]
-        null: bool,
-
-        #[arg(action = ArgAction::Append, long, short, help = "remove variable from the environment")]
-        unset: Vec<String>,
-
-        #[arg(help = "KEY=VALUE to set in the environment", value_parser = parse_kv_pair)]
-        kv_pair: Vec<String>,
-
-        #[arg(help = "command to run in the environment", last = true)]
-        // FIXME: this requires the command to be passed via `--`, which differs from coreutils env
-        command: Vec<String>,
-    },
-    #[command(about = "Convert tabs in each FILE to spaces, writing to standard output.")]
-    Expand {
-        #[arg(default_value = "-", help = "file to concatenate")]
-        file: String,
-
-        #[arg(long, short, value_name = "N,LIST", value_delimiter = ',', num_args = 1.., help = "have tabs N characters apart, not 8")]
-        tabs: Vec<String>,
-    },
-    False {},
-    #[command(about = "List information about the FILEs (the current directory by default).")]
-    Ls {
-        #[arg(short, long, help = "do not ignore entries starting with '.'")]
-        all: bool,
-
-        #[arg(help = "path to list", default_value = ".")]
-        path: String,
-    },
-    #[command(about = "Create directories if they do not already exist")]
-    Mkdir {
-        #[arg(required = true, help = "directories to create")]
-        dirs: Vec<PathBuf>,
-
-        #[arg(
-            short,
-            long,
-            env = "RZ_MKDIR_PARENTS",
-            help = "create parent directories as needed"
-        )]
-        parents: bool,
-    },
-    Nproc {
-        #[arg(
-            short,
-            long,
-            help = "print the number of cores available to the system"
-        )]
-        all: bool,
-
-        #[arg(
-            short,
-            long,
-            value_name = "N",
-            hide_default_value = true,
-            help = "ignore up to N cores",
-            default_value_t = 0
-        )]
-        ignore: usize,
-
-        #[arg(long, env = "OMP_NUM_LIMIT", help = "maximum threads to report")]
-        omp_num_limit: Option<usize>,
-
-        #[arg(long, env = "OMP_NUM_THREADS", help = "minimum threads to report")]
-        omp_num_threads: Option<usize>,
-    },
-    #[command(about = "A shell.")]
-    Sh {},
-    #[command(about = "Pause for NUMBER of seconds")]
-    Sleep {
-        #[arg(help = "NUMBER of seconds to sleep")]
-        number: String,
-    },
-    #[command(about = "Reduce a word to its stem")]
-    Stem {
-        #[arg(
-            long,
-            short,
-            default_value_t = false,
-            help = "do not output a trailing newline"
-        )]
-        nonewline: bool,
-
-        string: Vec<String>,
-    },
-    True {},
-    Uname {
-        #[arg(long, short, default_value_t = false, help = "print all information")]
-        all: bool,
-
-        #[arg(long, short = 's', help = "print the kernel name")]
-        kernel: bool,
-
-        #[arg(
-            long,
-            short = 'n',
-            default_value_t = false,
-            help = "print the network node hostname"
-        )]
-        nodename: bool,
-
-        #[arg(
-            long = "kernel-release",
-            short = 'r',
-            default_value_t = false,
-            help = "print the kernel release"
-        )]
-        kernel_release: bool,
-
-        #[arg(
-            long = "kernel-version",
-            short = 'v',
-            default_value_t = false,
-            help = "print the kernel version"
-        )]
-        kernel_version: bool,
-
-        #[arg(
-            long,
-            short,
-            default_value_t = false,
-            help = "print the machine hardware name"
-        )]
-        machine: bool,
-
-        #[arg(
-            long,
-            short,
-            default_value_t = false,
-            help = "print the operating system"
-        )]
-        operating_system: bool,
-
-        #[arg(
-            long,
-            short,
-            help = "the ISA format to use for CPU info",
-            default_value_t
-        )]
-        isa_format: IsaFormat,
-    },
-    Which {
-        #[arg(
-            short,
-            default_value_t = false,
-            help = "print all matching pathnames of each argument"
-        )]
-        all_occurrences: bool,
-
-        #[arg(help = "command to search for in PATH")]
-        command: String,
-
-        #[arg(
-            short,
-            default_value_t = false,
-            help = "silently return 0 if all of the executables were found or 1 otherwise"
-        )]
-        silent: bool,
-    },
-    Yes {
-        #[arg(
-            short,
-            long,
-            default_value = "0",
-            group = "yes_group",
-            help = "AMOUNT of TEXT to output"
-        )]
-        amount: usize,
-
-        #[arg(
-            short,
-            long,
-            group = "yes_group",
-            help = "output TEXT for a DURATION in seconds"
-        )]
-        // Optional<f32> to work around ArgGroup requiring one or the other Arg from the group
-        duration: Option<f32>,
-
-        #[arg(default_value = "y")]
-        text: String,
-    },
-}
+};
 
 #[allow(clippy::too_many_lines)]
 fn main() -> Result<()> {
@@ -542,8 +184,8 @@ fn main() -> Result<()> {
             Commands::Sleep { number } => {
                 sleep_command(&number)?;
             }
-            Commands::Stem { nonewline, string } => {
-                stem_command(nonewline, &string);
+            Commands::Stem { nonewline, words } => {
+                stem_command(nonewline, &words);
             }
             Commands::True {} => {
                 true_command();
